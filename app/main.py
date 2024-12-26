849b5e9f251595d2a5905f8c071b02befcc2dced
Tharak-N <tharak.n@innovatily.com>
2024-12-26 20:34:05 +0530

    adding basic embedding

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 
 from fastapi import FastAPI
 
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 app = FastAPI()
 
@@ -10,4 +11,7 @@ async def root():
 
 @app.get("/st_docker")
 async def sentence_transformer_docker():
+  embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+  embeddings = embed_model.get_text_embedding("Hello world!")
+  print(len(embeddings))
   pass
